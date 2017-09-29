796c2e46e509b847ffb17a349ba23a039bc7a63e
Vovanni <Vovanni96@gmail.com>
2017-09-29 19:45:08 +0300

    Добавлен шифратор - ПР1

---
+++ pr_1/WinFormPr1_crypter/WinFormPr1/Crypt.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace WinFormPr1
+{
+    class Crypt
+    {
+        private String Output;
+        private int alphabet_number;
+        private bool letter_is_big;
+        private bool not_a_letter = true;
+
+        public String input_for_crypt(String Key, String Input)
+        {
+            int[] Letters = new int[33];
+            int After_Key = 0;
+
+            for (int i = 0; i < Key.Length; i++)    //Записываем ключ в начало алфавитной таблицы без повторов одинаковых символов ключа
+            {
+                return_number(Key[i], false);
+                if (i == 0)
+                    {
+                        Letters[After_Key] = alphabet_number;
+                        After_Key++;
+                    }
+                else for (int n = 0; n < i; n++)
+                    {
+                        if (Letters[n] == alphabet_number) break;
+                        else if (n == i - 1)
+                        {
+                            Letters[After_Key] = alphabet_number;
+                            After_Key++;
+                        }
+                    }
+            }
+
+            for (int i = After_Key, k = 0; i < Letters.Length;) //Дозаписываем шифрованную таблицу алфавита
+            {
+                for (int n = 0; n < After_Key; n++)      //Сравниваем текущую букву с ключом
+                {
+                    if (Letters[n] == k) break;
+                    else if (n == After_Key-1)
+                    {
+                        Letters[i] = k;
+                        i++;
+                    }
+                }
+                k++;
+            }
+
+            for (int i=0; i < Input.Length; i++)
+            {
+                return_number(Input[i], true);
+                if (not_a_letter) Output += Input[i];
+                else Output += return_acsii(Letters[alphabet_number], letter_is_big);
+            }
+            return (Output);
+        }
+
+        private void return_number(int acsii_number, bool reg_is_need)  //Переводим ACSII код в код алфавита 0-32
+        {
+            if ((acsii_number <= 1103) & (acsii_number >= 1040))
+            {
+                if ((acsii_number <= 1071))
+                {
+                    alphabet_number = acsii_number - 1040;
+                    if (reg_is_need) letter_is_big = true;
+                }
+                else
+                {
+                    alphabet_number = acsii_number - 1072;
+                    if (reg_is_need) letter_is_big = false;
+                }
+                not_a_letter = false;
+                if (alphabet_number > 5) alphabet_number++;       //В ACSII символы Ё и ё идут не по порядку алфавита.
+            }
+            else if ((acsii_number == 1025) || (acsii_number == 1105))
+            {
+                alphabet_number = 6;
+                not_a_letter = false;
+            }
+            else not_a_letter = true;
+        }
+
+        private Char return_acsii(int alphabet_number, bool is_big) //Переводим код алфавита 0-32 в ACSII
+        {
+            int acsii_number;
+            if (is_big) if (alphabet_number == 6) acsii_number = 1025;
+                        else if (alphabet_number < 6) acsii_number = alphabet_number + 1040;
+                             else acsii_number = alphabet_number + 1039;
+            else if (alphabet_number == 6) acsii_number = 1105;
+                 else if (alphabet_number < 6) acsii_number = alphabet_number + 1072;
+                      else acsii_number = alphabet_number + 1071;
+            return ((Char)acsii_number);
+        }
+    }
+}
---
+++ pr_1/WinFormPr1_crypter/WinFormPr1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormPr1
+{
+    public partial class Form1 : Form
+    {
+        private String input_string;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stream input_stream;
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((input_stream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (StreamReader in_reader = new StreamReader(input_stream, Encoding.GetEncoding(1251))) //Прописываем энкодинг с русскими символами
+                        {
+                            input_string = in_reader.ReadToEnd();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Невозможно прочитать файл с диска. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stream output_stream;
+            String Key;
+            if ((Key = textBox1.Text) != null)
+            {
+                if (Key.Length <= 33)
+                {
+                    if (input_string != null)
+                    {
+                        Crypt To_Crypt = new Crypt();
+
+                        saveFileDialog1.InitialDirectory = "c:\\";
+                        saveFileDialog1.RestoreDirectory = true;
+                        if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                        {
+                            try
+                            {
+                                if ((output_stream = saveFileDialog1.OpenFile()) != null)
+                                {
+                                    using (StreamWriter out_writer = new StreamWriter(output_stream, Encoding.GetEncoding(1251))) //Прописываем энкодинг с русскими символами
+                                    {
+                                        out_writer.Write(To_Crypt.input_for_crypt(Key, input_string));
+                                    }
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Error: Невозможно записать в файл. Original error: " + ex.Message);
+                            }
+                        }
+                    }
+                    else MessageBox.Show("Error: Входной файл не выбран или пуст.");
+                }
+                else MessageBox.Show("Error: Ключ превышает длину алфавита.");
+            }
+        }
+
+    }
+}
