5b6d72c95587c278f6a124a011f5fee3f86e4913
Vovanni <Vovanni96@gmail.com>
2017-11-07 15:25:16 +0300

    Добавлена ПР3 Протокол Диффи - Хеллмана

---
+++ pr_3/WindowsFormPr3_Diffie/WindowsFormPr2_Diffie/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormPr2_Diffie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(339, 425);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 264);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(321, 205);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Алиса";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(250, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ева";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(253, 30);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(321, 205);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(514, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Боб";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(517, 264);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(321, 205);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormPr2_Diffie.Properties.Resources._1600_1;
+            this.pictureBox2.Location = new System.Drawing.Point(33, 30);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(195, 160);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormPr2_Diffie.Properties.Resources._1600;
+            this.pictureBox1.Location = new System.Drawing.Point(595, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(218, 160);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 481);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Диффи Хеллман";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ pr_3/WindowsFormPr3_Diffie/WindowsFormPr2_Diffie/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormPr2_Diffie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PrimeWorker Forecer = new PrimeWorker();
+            Forecer.get_two_primes();
+            uint g = Forecer.min_prime;
+            ulong p = Forecer.max_prime;
+            p = 2*p+1;                                //Умножаем p на 2 и прибавляем 1 для безопасности
+
+            uint a = 0, b = 0;
+            ulong A = 0, B = 0;
+            ulong KA = 0, KB = 0;
+
+            ThreadStart core_1_start = new ThreadStart(get_A);
+            ThreadStart core_2_start = new ThreadStart(get_B);
+            Thread th_1 = new Thread(core_1_start);
+            Thread th_2 = new Thread(core_2_start);
+            th_1.IsBackground = true;
+            th_2.IsBackground = true;
+            th_1.Start();
+            th_2.Start();
+            while (th_1.IsAlive || th_2.IsAlive);
+
+            richTextBox1.Text += "Алиса генерирует a,g,p:\na=" + a + ";\ng=" + g + ";\np=" + p + ";\n";
+            richTextBox3.Text += "Боб генерирует b:\nb=" + b + ";\n";
+
+            richTextBox1.Text += "Алиса вычисляет A = (g^a) mod p:\nA=" + A +";\n" + "Алиса отправлет Бобу A, g, p;\n";
+            richTextBox2.Text += "Ева перехватывает от Алисы A,g,p:\nA=" + A + ";\ng=" + g + ";\np=" + p + ";\n";
+            richTextBox3.Text += "Боб получает от Алисы A,g,p:\nA=" + A + ";\ng=" + g + ";\np=" + p + ";\n";
+
+            richTextBox3.Text += "Боб вычисляет B = (g^b) mod p:\nB=" + B +";\n" + "Боб отправлет Алисе B;\n";
+            richTextBox2.Text += "Ева перехватывает от Боба B:\nB=" + B + ";\n";
+            richTextBox1.Text += "Алиса получает от Боба B:\nB=" + B + ";\n";
+
+            ThreadStart core_3_start = new ThreadStart(get_KA);
+            ThreadStart core_4_start = new ThreadStart(get_KB);
+            Thread th_3 = new Thread(core_3_start);
+            Thread th_4 = new Thread(core_4_start);
+            th_3.IsBackground = true;
+            th_4.IsBackground = true;
+            th_3.Start();
+            th_4.Start();
+            while (th_3.IsAlive || th_4.IsAlive);
+
+            richTextBox3.Text += "Боб вычисляет закрытый ключ K = (A^b) mod p:\nK=" + KB + ";\n";
+            richTextBox1.Text += "Алиса вычисляет закрытый ключ K = (B^a) mod p:\nK=" + KA + ";\n";
+
+            void get_A()
+            {
+                a = Forecer.UintLessRand(999999);
+                A = Forecer.powm(g, a, p);
+            }
+
+            void get_B()
+            {
+                b = Forecer.UintLessRand(999999);
+                B = Forecer.powm(g, b, p);
+            }
+
+            void get_KA()
+            {
+                KA = Forecer.powm(B, a, p);
+            }
+
+            void get_KB()
+            {
+                KB = Forecer.powm(A, b, p);
+            }
+        }
+    }
+
+    public class PrimeWorker
+    {
+        private Random randomizer = new Random();
+        private int max;
+        public uint min_prime;
+        public uint max_prime;
+
+        public ulong powm(ulong a, ulong b, ulong n)        //a^b mod n
+        {
+            ulong c = 1;
+            while (b != 0)
+            {
+                if (b % 2 == 0)
+                {
+                    b /= 2;
+                    a = (a * a) % n;
+                }
+                else
+                {
+                    b--;
+                    c = (c * a) % n;
+                }
+            }
+            return c;
+        }
+
+        public void get_two_primes()
+        {
+            get_min_prime();
+            get_max_prime();
+            if (min_prime>max_prime)
+            {
+                var buf = min_prime;
+                min_prime = max_prime;
+                max_prime = buf;
+            }
+        }
+
+        private void get_max_prime()
+        {
+            set_max(1999999999);
+            max_prime = prime_gen(UlongRand());
+        }
+
+        private void get_min_prime()
+        {
+            set_max(7);
+            min_prime = prime_gen(UlongRand());
+        }
+
+        public void set_max(int n)
+        {
+            max = n;
+        }
+
+        private uint prime_gen(uint number)                              //Генератор простого числа
+        {
+            var local_num = number;
+            if (divide(local_num, 2)) local_num++;
+            while (!is_prime(local_num)) local_num += 2;
+            return local_num;
+        }
+
+        private bool is_prime(uint number)
+        {
+            return is_prime_core(number, 3, number / 2 + 1);
+        }
+
+        private bool is_prime_core(uint number, uint k, uint n)            //Проверка на простоту
+        {
+            for (uint i = k; i <= n; i += 2)
+            {
+                if (divide(number, i)) return false;
+            }
+            return true;
+        }
+
+        private bool divide(uint num, uint denom)                         //Проверка на делимость без остатка
+        {
+            if ((num % denom) == 0) return true;
+            else return false;
+        }
+
+        private uint UlongRand()                                           //Сгенерировать число
+        {
+            return UintLessRand(max);                                     //Можно до 2147483647
+        }
+
+        public uint UintLessRand(int A)                                   //Получить число меньшее А
+        {
+            return (uint)randomizer.Next(3, A);
+        }
+    }
+}
+    
